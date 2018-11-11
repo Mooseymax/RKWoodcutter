6aff3d77710a4a6cb324f204ee7bfafc14cf0a08
Mooseymax <mooseandham@gmail.com>
2018-11-11 02:49:11 +0000

    Update src/Woodcutter.java

--- src/Woodcutter.java
+++ src/Woodcutter.java
@@ -1,3 +1,13 @@
-public class Woodcutter {
-    
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+
+@ScriptManifest(author = "OSRunekeep", name = "RK Tutorial", info = "Tutorial Island Buster", version = 0.1, logo = "")
+
+public final class Woodcutter extends Script {
+
+    @Override
+    public final int onLoop() throws InterruptedException {
+        return 0;
+    }
+
 }
