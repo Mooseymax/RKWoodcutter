74f53b32f9f8499ed14f720bba5665382442e6bf
Mooseymax <mooseandham@gmail.com>
2018-11-11 03:35:39 +0000

    Basics

--- src/Woodcutter.java
+++ src/Woodcutter.java
@@ -1,7 +1,8 @@
+import org.osbot.rs07.api.ui.Message;
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 
-@ScriptManifest(author = "OSRunekeep", name = "RK Tutorial", info = "Tutorial Island Buster", version = 0.1, logo = "")
+@ScriptManifest(author = "OSRunekeep", name = "RK Woodcutter", info = "Simple Woodcutter", version = 0.1, logo = "")
 
 public final class Woodcutter extends Script {
 
@@ -12,12 +13,20 @@ public final class Woodcutter extends Script {
 
     @Override
     public final void onStart() {
-        log("On Start Test");
+        log("On Start Test.");
+        log("Jed sucks dick.")
     }
 
     @Override
-    public final void OnExit() {
+    public final void onExit() {
         log("On Exit Test");
     }
+
+    @Override
+    public void onMessage(Message message) throws InterruptedException {
+        super.onMessage(message);
+    }
+
+    
 
 }
