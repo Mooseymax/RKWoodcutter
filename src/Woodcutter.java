c6843af3a2be4d23d87dd5ff21a42fc278b68695
Mooseymax <mooseandham@gmail.com>
2018-11-11 04:22:17 +0000

    Update src/Woodcutter.java

--- src/Woodcutter.java
+++ src/Woodcutter.java
@@ -1,6 +1,8 @@
+import org.osbot.rs07.api.model.Entity;
 import org.osbot.rs07.api.ui.Message;
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
+import org.osbot.rs07.utility.ConditionalSleep;
 
 @ScriptManifest(author = "OSRunekeep", name = "RK Woodcutter", info = "Simple Woodcutter", version = 0.1, logo = "")
 
@@ -8,13 +10,15 @@ public final class Woodcutter extends Script {
 
     @Override
     public final int onLoop() throws InterruptedException {
+
         return 0;
     }
 
     @Override
     public final void onStart() {
         log("On Start Test.");
-        log("Jed sucks dick.")
+        log("Max sucks dick.");
+        chopTree();
     }
 
     @Override
@@ -24,9 +28,17 @@ public final class Woodcutter extends Script {
 
     @Override
     public void onMessage(Message message) throws InterruptedException {
-        super.onMessage(message);
+        log("New message: " + message.getMessage());
+        // IDK what super.onMessage does lol
+        //super.onMessage(message);
     }
 
-    
+    private void chopTree() {
+        Entity tree = getObjects().closest("Tree");
+        if(tree != null && tree.interact("Chop down")){
+            // Chop down tree
+            tree.interact("Chop down");
+        }
+    }
 
 }
